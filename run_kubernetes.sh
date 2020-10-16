a6d7504c2fa5f5d0ac7e1672fc8c4f9fa0556421
Ali1424 <71447605+Ali1424@users.noreply.github.com>
2020-10-16 21:16:03 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,4 +1,4 @@
-dockerpath= Ali1424/project4 
+dockerpath= udacitydevopsali/project4 
 kubectl run udacity-p4 --image=$dockerpath --port=80
 kubectl get pods
 kubectl port-forward deployment/udacity-p4 8080:80
