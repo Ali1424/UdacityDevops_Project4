5cfe0966cbe902e17b1c1b878559431aa8925051
Ali1424 <71447605+Ali1424@users.noreply.github.com>
2020-10-16 20:36:11 +0200

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,7 +1,7 @@
-dockerpath= Ali1424/project4 
+dockerpath= udacitydevopsali/project4
 
 export DOCKER_ID_USER="Ali1424"
 docker login
 docker tag project4 $DOCKER_ID_USER/project4
 docker push $DOCKER_ID_USER/project4
-docker push Ali1424/project4
+docker push udacitydevopsali/project4
